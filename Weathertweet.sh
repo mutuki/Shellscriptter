e1f72d1f307da6027ac0baca6d9abe3a2cf6e3cb
Hiroshi <mutuki@mac.com>
2018-11-11 13:25:11 +0900

    Update Weathertweet.sh

--- Weathertweet.sh
+++ Weathertweet.sh
@@ -36,11 +36,12 @@ fi
 
 SUNRISE=`echo "$MASTERDATA" | grep -e sys | tr ',' '\n' | tr -d '"' | tr -d '}' | grep -e sunrise | tr -d 'sunrise:'`
 SUNSET=`echo "$MASTERDATA" | grep -e sys | tr ',' '\n' | tr -d '"' | tr -d '}' | grep -e sunset | tr -d 'sunset:'`
+CURRENTSKY=`date +%s`
 
 if [ ${debug:-off} = "on" ]; then
-echo -e "DAYLIGHT time information :\n$SUNRISE to $SUNSET"
+echo -e "DAYLIGHT time information :\n$SUNRISE to $SUNSET and current time is $CURRENTSKY"
 	
-	if [ $SUNRISE -gt `date +%s` ] && [ `date +%s` -gt $SUNSET ]; then
+	if [ $SUNRISE -lt $CURRENTSKY ] && [ $CURRENTSKY -lt $SUNSET ]; then
 		echo "will sunrise."
 	else
 		echo "will sunset."	
@@ -48,10 +49,10 @@ echo -e "DAYLIGHT time information :\n$SUNRISE to $SUNSET"
 
 fi
 
-if [ $SUNRISE -gt `date +%s` ] && [ `date +%s` -gt $SUNSET ]; then
-	WEATHER=`echo "$WEATHER" | sed -e 's/2[0-9]*/⛈/g' -e 's/3[0-9]*/☂️/g' -e 's/5[0-9]*/☔️/g' -e 's/6[0-9]*/❄️/g' -e 's/7[0-9]*/🌫/g' -e 's/800/☀️/g' -e 's/801/🌤/g' -e 's/802/⛅️/g' -e 's/803/🌥/g' -e 's/804/☁️/g'| head -n 1`
+if [ $SUNRISE -lt `date +%s` ] && [ `date +%s` -lt $SUNSET ]; then
+	WEATHER=`echo "$WEATHER" | sed -e 's/2[0-9][0-9]/⛈/g' -e 's/3[0-9][0-9]/☂️/g' -e 's/5[0-9][0-9]/☔️/g' -e 's/6[0-9[0-9]*/❄️/g' -e 's/7[0-9][0-9]/🌫/g' -e 's/800/☀️/g' -e 's/801/🌤/g' -e 's/802/⛅️/g' -e 's/803/🌥/g' -e 's/804/☁️/g'| head -n 1`
 else
-	WEATHER=`echo "$WEATHER"	| sed -e 's/2[0-9]*/⛈/g' -e 's/3[0-9]*/☂️/g' -e 's/5[0-9]*/☔️/g' -e 's/6[0-9]*/❄️/g' -e 's/7[0-9]*/🌫/g' -e 's/8[0-9]*/🌕/g' | head -n 1`
+	WEATHER=`echo "$WEATHER"	| sed -e 's/2[0-9][0-9]/⛈/g' -e 's/3[0-9][0-9]/☂️/g' -e 's/5[0-9][0-9]/☔️/g' -e 's/6[0-9[0-9]*/❄️/g' -e 's/7[0-9][0-9]/🌫/g' -e 's/8[0-9]*/🌕/g' | head -n 1`
 fi
 
 TEMPERATURE=`echo "$MASTERDATA" | grep -e base | tr ',' '\n' | tr -d '"' | tr -d '}' | grep -e main | sed -e 's/main:{temp://'`
